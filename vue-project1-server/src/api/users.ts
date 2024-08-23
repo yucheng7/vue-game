9891aa2f50abd4391e3131c7d5d4a39d5a0ca644
YuCh7_on_macbook <aa139532058@gmail.com>
2024-08-24 04:47:16 +0800

    new feature

--- vue-project1-server/src/api/users.ts
+++ vue-project1-server/src/api/users.ts
@@ -1,4 +1,17 @@
+import { log } from 'console';
 import express, { Request, Response, Application, Router } from 'express';
+import mongoose from 'mongoose';
+const url: string = "mongodb+srv://aa102133395:R3V0a7AqqkL4MoaK@cluster0.vunkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(url, {})
+.then((res) => {
+    console.log("已連線到資料庫");
+    
+})
+.catch((err) => {
+    console.log(err);
+    
+})
+
 
 
 const userRoutes: Router = express.Router();
